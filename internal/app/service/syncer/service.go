0cd84607a2ebc62ea9fe6d4a8f1f920deb85b05c
Alexey Samoylov <alexey.samoylov@gmail.com>
2021-11-08 19:08:11 +0700

    reduced timeouts

--- internal/app/service/syncer/service.go
+++ internal/app/service/syncer/service.go
@@ -58,7 +58,7 @@ func New(db *sql.DB, ac *accrual.Service) (*Service, error) {
 		accrual: ac,
 		db:      db,
 
-		fetchInterval: 1 * time.Second,
+		fetchInterval: 500 * time.Millisecond,
 		jobTimeout:    30 * time.Second,
 	}
 	s.Start(runtime.GOMAXPROCS(0))
