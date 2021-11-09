98c015a7b020657b2e91c634c771fdff7c8fd167
Alexey Samoylov <alexey.samoylov@gmail.com>
2021-11-09 19:20:01 +0700

    order marshaller hack

--- internal/app/model/order.go
+++ internal/app/model/order.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
 	"time"
@@ -13,4 +14,21 @@ type Order struct {
 	UserID     uuid.UUID           `json:"-"`
 	Status     string              `json:"status"`
 	Accrual    decimal.NullDecimal `json:"accrual,omitempty"`
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (d Order) MarshalJSON() ([]byte, error) {
+	o := struct {
+		ExternalID string    `json:"number"`
+		CreatedAt  time.Time `json:"uploaded_at"`
+		Status     string    `json:"status"`
+		Accrual    float64   `json:"accrual,omitempty"`
+	}{
+		ExternalID: d.ExternalID,
+		CreatedAt:  d.CreatedAt,
+		Status:     d.Status,
+		Accrual:    d.Accrual.Decimal.InexactFloat64(),
+	}
+
+	return json.Marshal(o)
 }
